4b16ce625d9bbd38fc4995009daa21a3cc059443
Vittor-Javidan <vittor.javidan.t@gmail.com>
2023-03-13 18:32:29 -0300

    REMOVED: Unecessary text inside translation files

--- app/GeneralSettings/translations.ts
+++ app/GeneralSettings/translations.ts
@@ -2,17 +2,11 @@ import { Languages } from "../../Classes/Settings";
 
 export const languages: Record<Languages, {
     "General Settings": string,
-    "This is the second page of your app.": string,
-    "Back Home": string
 }> = {
     "en-US": {
         "General Settings": "General Settings",
-        "This is the second page of your app.": "This is the second page of your app.",
-        "Back Home": "Back Home"
     },
     "pt-BR": {
-        "General Settings": "Configurações",
-        "This is the second page of your app.": "Essa é a segunda página do seu app",
-        "Back Home": "Voltar ao início"
+        "General Settings": "Configurações Gerais",
     }
 }
