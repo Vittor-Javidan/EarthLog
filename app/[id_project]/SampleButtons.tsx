79cdc141be041b82a42391d9678671fd69339073
Vittor-Javidan <vittor.javidan.t@gmail.com>
2023-08-04 07:59:23 -0300

    Sample subtitle removed from Project Screen

--- app/[id_project]/SampleButtons.tsx
+++ app/[id_project]/SampleButtons.tsx
@@ -1,37 +1,11 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { useLocalSearchParams } from 'expo-router';
 import { Layout } from '@Components/Layout';
 
-import ThemeService from '@Services/ThemeService';
 import ProjectService from '@Services/ProjectService';
 import { useNavigate } from 'app/GlobalHooks';
-import ConfigService from '@Services/ConfigService';
-import { translations } from '@Translations/index';
 
 export default function SampleButtons() {
-
-  const stringResources = useMemo(
-    () => translations.Screens.ProjectScreen[ConfigService.config.language], []
-  );
-
-  const showSamples = ProjectService.allSamples.length > 0;
-
-  return (
-    <Layout.View>
-      {showSamples && (<>
-          <Layout.Text
-            fontSize={ThemeService.FONTS.h2}
-            color={'onBackground'}
-          >
-            {stringResources['Samples']}
-          </Layout.Text>
-          <AllButtons />
-        </>)}
-    </Layout.View>
-  );
-}
-
-function AllButtons() {
 
   const id_project = useLocalSearchParams().id_project as string;
 
