0432120efdf48e80e8ee1afe50e09c6c9b862431
Vittor-Javidan <vittor.javidan.t@gmail.com>
2023-08-13 16:31:48 -0300

    Screen Buttons background removed

--- src/Components/Layout/ScreenButtons.tsx
+++ src/Components/Layout/ScreenButtons.tsx
@@ -1,27 +1,11 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { View } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import ConfigService from '@Services/ConfigService';
 
 export default function ScreenButtons(props: {
   button_left?: JSX.Element
   button_middle?: JSX.Element
   button_right?: JSX.Element
 }) {
-
-  const { theme } = useMemo(() => ConfigService.config, [ConfigService.config.theme]);
-
-  const COLORS = useMemo<string[]>(() => [
-    'transparent',
-    'transparent',
-    theme.background,
-    theme.background,
-    theme.background,
-    theme.background,
-    theme.background,
-    theme.background,
-    theme.background,
-  ], []);
 
   const HORIZONTAL_PADDING = 10;
 
@@ -69,26 +53,6 @@ export default function ScreenButtons(props: {
         >
           {props.button_right}
         </View>
-      </View>
-      <View
-        style={{
-          position: 'absolute',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          width: '100%',
-          bottom: 0,
-          paddingTop: 10,
-        }}
-      >
-        <LinearGradient
-          colors={COLORS}
-          style={{
-            height:120,
-            width: '100%',
-            zIndex: 5,
-          }}
-        />
       </View>
     </>
   );
