9d8cd68ff87a0bdcb506f26014448d0904781b9d
Vittor-Javidan <vittor.javidan.t@gmail.com>
2023-08-13 23:24:27 -0300

    SwipeButton slot added to ScreenButtons component

--- src/Components/Layout/ScreenButtons.tsx
+++ src/Components/Layout/ScreenButtons.tsx
@@ -5,6 +5,8 @@ export default function ScreenButtons(props: {
   button_left?: JSX.Element
   button_middle?: JSX.Element
   button_right?: JSX.Element
+  showSwipe?: boolean
+  SwipeButton?: JSX.Element
 }) {
 
   const HORIZONTAL_PADDING = 10;
@@ -24,35 +26,39 @@ export default function ScreenButtons(props: {
           zIndex: 10,
         }}
       >
-        <View
-          style={{
-            width: '33%',
-            justifyContent: 'center',
-            alignItems: 'flex-start',
-            paddingLeft: HORIZONTAL_PADDING,
-          }}
-        >
-          {props.button_left}
-        </View>
-        <View
-          style={{
-            width: '33%',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          {props.button_middle}
-        </View>
-        <View
-          style={{
-            width: '33%',
-            justifyContent: 'center',
-            alignItems: 'flex-end',
-            paddingRight: HORIZONTAL_PADDING,
-          }}
-        >
-          {props.button_right}
-        </View>
+        {props.showSwipe ? (<>
+          {props.SwipeButton}
+        </>) : (<>
+          <View
+            style={{
+              width: '33%',
+              justifyContent: 'center',
+              alignItems: 'flex-start',
+              paddingLeft: HORIZONTAL_PADDING,
+            }}
+          >
+            {props.button_left}
+          </View>
+          <View
+            style={{
+              width: '33%',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            {props.button_middle}
+          </View>
+          <View
+            style={{
+              width: '33%',
+              justifyContent: 'center',
+              alignItems: 'flex-end',
+              paddingRight: HORIZONTAL_PADDING,
+            }}
+          >
+            {props.button_right}
+          </View>
+        </>)}
       </View>
     </>
   );
